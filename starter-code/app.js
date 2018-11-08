c5fe3bdf121d36a3e89d985d83aa284718be40d2
“yannyyss” <yannyyss@hotmail.com>
2018-11-07 18:18:42 -0600

    Reviewed and with comments added

--- starter-code/app.js
+++ starter-code/app.js
@@ -1,10 +1,12 @@
 const express = require('express');
-
 const app = express();
 
+// Make everything  inside of public directory avaliable:
 app.use(express.static(__dirname + '/public'));
 
-app.get('/',(req,res)=>{
+//Routes:
+
+app.get('/', (req, res) => {
     res.sendFile(__dirname + '/index.html');
 });
 
@@ -12,11 +14,11 @@ app.get('/about', (req, res) => {
     res.sendFile(__dirname + '/public/about.html');
 });
 
-app.get("/fotos", (req, res) => {
+app.get('/fotos', (req, res) => {
     res.sendFile(__dirname + '/public/fotos.html');
 });
 
-
+//Server started:
 app.listen(3000,()=>{
     console.log('app escuchando 3000');
 });
